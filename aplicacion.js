5fdc1899bf35013fc6695fa0250ca0253110f5cc
Maria <gpe.felix14@gmail.com>
2020-09-26 18:26:19 -0600

    Cambia aplicacion

---
+++ aplicacion.js
@@ -0,0 +1,15 @@
+javascript_include_tag :
+application, 
+( admin if [[condition]])
+
+or
+
+ app/views/layouts/admin.html.erb
+
+javascript_include_tag :
+admin 
+<head>
+   <title>Course</title>
+   
+ </head>
+ <body></body>
